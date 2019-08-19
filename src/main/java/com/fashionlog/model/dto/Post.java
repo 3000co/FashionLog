51f46eca0c9b8cd744dfc39da7a6a14a936f4ba7
haneol <n0netheless@naver.com>
2019-08-19 16:34:27 +0900

    db조작 테스트
    post insert성공

--- src/main/java/com/fashionlog/model/dto/Post.java
+++ src/main/java/com/fashionlog/model/dto/Post.java
@@ -1,5 +1,30 @@
 package com.fashionlog.model.dto;
 
-public class Post {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @ToString
+public class Post {
+	@Id
+	@GeneratedValue
+	private int postNo;
+	private int memberNo;
+	private Date uploadTime;
+	private int postImageNo;
+	private String contents;
+	private int styleNo1;
+	private int styleNo2;
+	private int styleNo3;
+	
+	
+	
+	
 }
