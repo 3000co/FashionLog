e56b975d27bacf48d2fb09e09ee9d8f996147b56
Bran <june20516@gmail.com>
2019-08-22 13:58:26 +0900

    템플릿 폴더로 분류, 노티 컨트롤러 작성 시작

--- src/main/java/com/fashionlog/controller/NotificationController.java
+++ src/main/java/com/fashionlog/controller/NotificationController.java
@@ -1,9 +1,37 @@
 package com.fashionlog.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fashionlog.model.dao.NotificiationRepository;
+import com.fashionlog.model.dto.Notification;
 
 @Controller
 public class NotificationController {
+	private NotificiationRepository notificiationRepository;
+	
+	@RequestMapping("/noti/{memberNo}")
+	public String notificationList(@PathVariable int memberNo, Model model) {
+		
+		List<Notification> uncheckedNoti = notificiationRepository.findByRecieverMemNo(memberNo);
+		model.addAllAttributes(uncheckedNoti);
+	
+		return "notification/notification";
+	}
+	
+	@RequestMapping("/noti/all")
+	@ResponseBody
+	public List<Notification> notificationAll() {
+		
+		List<Notification> AllNotiResult = notificiationRepository.findAll();
+		return AllNotiResult;
+		
+	}
 	
 	
 }
--- src/main/java/com/fashionlog/model/dao/NotificiationRepository.java
+++ src/main/java/com/fashionlog/model/dao/NotificiationRepository.java
@@ -2,12 +2,15 @@ package com.fashionlog.model.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.fashionlog.model.dto.Notification;
 
-public interface NotificiationRepository extends CrudRepository<Notification, Integer> {
+public interface NotificiationRepository extends JpaRepository<Notification, Integer> {
 
-	public List<Notification> findByRecieverMemNoAndCheckTimeIsNull(int RecieverMemNo);
+	public List<Notification> findByRecieverMemNoAndCheckTimeIsNull(int recieverMemNo);
+	
+	public List<Notification> findByRecieverMemNo(int recieverMemNo);
 	
 }
