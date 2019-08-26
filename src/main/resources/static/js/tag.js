a84240bdf406bb63912d691b028a672863b054bd
Yeon <sy423513@gmail.com>
2019-08-26 20:45:24 +0900

    css수정

--- src/main/resources/static/js/tag.js
+++ src/main/resources/static/js/tag.js
@@ -1,34 +1,33 @@
-$(document).ready(
-	function() {
-		var count = 0;
-		var newTag = $("#tagContents1");
-		var tagMold;
-		
-		$(document).on("click","#img",function(event) {
-			count++;
-			if (count <= 7) {
-				var eventX = event.clientX;
-				var eventY = event.clientY;
-				var tagStyle = $(newTag).css("display");
+$(document).ready( function() {
+	var count = 0;
+	var newTag = $("#itemTag1");
+	var tagMold;
+	
+	$(document).on("click", "#img", function(event) {
+		count++;
+		if (count <= 7) {
+			var eventX = event.clientX;
+			var eventY = event.clientY;
+			var tagStyle = $(newTag).css("display");
 
-				if (tagStyle === "block") {
-					newTag = $(tagMold).clone().attr("id", "tagContents" + count);
-					$("#tagWrap").append(newTag);
-					$(newTag).find('#xCoordinate').val(eventX);
-					$(newTag).find('#yCoordinate').val(eventY);
-				} else {
-					$(newTag).show();
-					tagMold = $(newTag).clone();
-					$('#xCoordinate').val(eventX);
-					$('#yCoordinate').val(eventY);
-				}
+			if (tagStyle === "block") {
+				newTag = $(tagMold).clone().attr("id", "itemTag" + count);
+				$("#itemTagWrap").append(newTag);
+				$(newTag).find('#xCoordinate').val(eventX);
+				$(newTag).find('#yCoordinate').val(eventY);
+			} else {
+				$(newTag).show();
+				tagMold = $(newTag).clone();
+				$('#xCoordinate').val(eventX);
+				$('#yCoordinate').val(eventY);
 			}
-		});
-		$(document).on("click","#exitBtn",function(event) {
-			console.log(event.target);
-			console.log($(event.target).parents(".tagContents"));
-			$(event.target).parents(".tagContents").hide();
-			count--;
-		});
-
+		}
+	});
+	$(document).on("click", "#exitBtn", function(event) {
+		console.log(event.target);
+		console.log($(event.target).parents(".itemTag"));
+		$(event.target).parents(".itemTag").hide();
+		count--;
+	});
+	
 });
