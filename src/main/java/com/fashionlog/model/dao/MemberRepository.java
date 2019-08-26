6469572a53bb8b42812106b550714886be2199ec
chaewonkim <kimchaewon1004@gmail.com>
2019-08-26 17:10:25 +0900

    AFTER MERGE CW
    member findById(String) 복구

--- src/main/java/com/fashionlog/model/dao/MemberRepository.java
+++ src/main/java/com/fashionlog/model/dao/MemberRepository.java
@@ -6,6 +6,8 @@ import com.fashionlog.model.dto.Member;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 	public Member findByIdAndPassword(String Id, String Password);
+	
+	public Member findById(String Id);
 
 }
 
