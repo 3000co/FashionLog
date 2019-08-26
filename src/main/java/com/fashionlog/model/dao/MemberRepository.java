92424c9ed0f8976da5b0d0ecfd0fb5eecb43ff9f
chaewonkim <kimchaewon1004@gmail.com>
2019-08-26 15:23:02 +0900

    0826

--- src/main/java/com/fashionlog/model/dao/MemberRepository.java
+++ src/main/java/com/fashionlog/model/dao/MemberRepository.java
@@ -3,20 +3,21 @@ package com.fashionlog.model.dao;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.fashionlog.model.dto.Member;
 
-public interface MemberRepository extends CrudRepository<Member, Integer> {
-	@Query(value = "select id from Member where id= ${id} and password=${password}" , nativeQuery = true)
-	public List<Member> findById(String id);
+public interface MemberRepository extends JpaRepository<Member, Integer> {
 	
-	@Query(value= "insert into Member(id,password,nickname,phonenumber,email,profile_Image_No,style_No1) values (id,'1','1','1','1','1','1')",nativeQuery = true)
+	public Member findById(String id);
+	
+	@Query(value= "insert into Member(id,password,nickname,phonenumber,email,profile_Image_No,style_No1) values (#{id},'1','1','1','1','1','1')",nativeQuery = true)
 	public void setJoin(Member member);
 	
-	@Query(value="select id from Member")
-	public Member getMemberInfo(Member member);
+	
+//	public Member getMemberInfo(Member member);
 
 		
 	
