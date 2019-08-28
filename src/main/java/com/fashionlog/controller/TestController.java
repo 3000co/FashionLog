d2067c8a1d6eb519564eeb6be8a6c9e84e00cf80
haneol <n0netheless@naver.com>
2019-08-28 18:18:16 +0900

    8.29
    메뉴변경해서 토큰 등록 가능

---
+++ src/main/java/com/fashionlog/controller/TestController.java
@@ -0,0 +1,40 @@
+package com.fashionlog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fashionlog.model.dao.BrandRepository;
+import com.fashionlog.model.dao.CategoryRepository;
+import com.fashionlog.model.dao.StyleRepository;
+import com.fashionlog.model.dto.Category;
+import com.fashionlog.model.dto.Style;
+
+
+
+@Controller
+public class TestController {
+	@Autowired
+	private StyleRepository styleRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private BrandRepository brandRepository;
+	
+	@RequestMapping("/test")
+	public String startTest(Model model) {
+		//DB에서 목록을 가져와서 SelectBox에 이용
+		List<Style> style = styleRepository.findAll();
+		List<Category> category = categoryRepository.findAll();
+		List<Object[]> brand = brandRepository.findBrandQuery();
+		
+		model.addAttribute("style", style);
+		model.addAttribute("category", category);
+		model.addAttribute("brand", brand);
+		
+		return "view";
+	}
+}
