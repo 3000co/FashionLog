112460924fddaab7aeff1d10eae4c6f59d7d78a6
haneol <n0netheless@naver.com>
2019-09-04 11:41:11 +0900

    0904-11:40
    검색어 문자 처리 완료(예외 처리)

--- src/main/java/com/fashionlog/controller/TestController.java
+++ src/main/java/com/fashionlog/controller/TestController.java
@@ -45,55 +45,53 @@ public class TestController {
 		return "view";
 	}
 
-	
-	  @RequestMapping("/searchTest") 
-	  public String startTest2(HttpServletRequest request){ 
-		  ArrayList<String> searchTokenList=new ArrayList<>();
-		  ArrayList<String[]> searchTokenArrayList=new ArrayList<>();
-		  
-		  splitWordsByAmp(request.getParameter("searchWords"), searchTokenList);
-		  splitWordsByColon(searchTokenList,searchTokenArrayList);
-		  
-		  System.out.println(searchTokenArrayList);
-		  
-		  for(int i=0; i<=searchTokenArrayList.size()-1;i++) {
-			  String[] temp = searchTokenArrayList.get(i);
-			  for(String t:temp) {
-				  System.out.println(t);
-			  }
-		  }
-		  
-		  System.err.println(itemRepository.getItemPost("검은", "겨울"));
-		  
-		  return "view"; 
-	 }
-	  
-	  private List<String> splitWordsByAmp(String searchWords, ArrayList<String> searchTokenList){
-		  String[] searchTokenArray;
-		 
-		  
-		  searchTokenArray = searchWords.split("&");
-		  
-		  for(String temp:searchTokenArray) {
-			  searchTokenList.add(temp);
-			  		  }
-		  return searchTokenList;
-	  }
-	  
-	  private List<String[]> splitWordsByColon(ArrayList<String> searchTokenList, ArrayList<String[]> searchTokenArrayList){
+	@RequestMapping("/searchTest")
+	public String startTest2(HttpServletRequest request) {
+		ArrayList<String> searchTokenList = new ArrayList<>();
+		ArrayList<String[]> searchTokenArrayList = new ArrayList<>();
+
+		splitWordsByAmp(request.getParameter("searchWords"), searchTokenList);
+		splitWordsByColon(searchTokenList, searchTokenArrayList);
 		
-		  for(String temp: searchTokenList) {
-			  String[] searchToken = new String[2];
-			  searchToken= temp.split(":");
-			  searchTokenArrayList.add(searchToken);
+		  for (int i = 0; i <= searchTokenArrayList.size() - 1; i++) { String[] temp =
+		  searchTokenArrayList.get(i); for (String t : temp) { System.out.println(t); }
 		  }
-		  
-		  
-		  return searchTokenArrayList;
-	  }
-	  
-	  
-	  
-	 
-	 
+		 
+
+		System.err.println(itemRepository.getItemPost("검은", "겨울"));
+
+		return "view";
+	}
+
+	private List<String> splitWordsByAmp(String searchWords, ArrayList<String> searchTokenList) {
+		String[] searchTokenArray;
+
+		searchTokenArray = searchWords.split("&");
+
+		
+		 for(String temp:searchTokenArray) {
+			 searchTokenList.add(temp); }
+		 
+		return searchTokenList;
+	}
+
+	private List<String[]> splitWordsByColon(ArrayList<String> searchTokenList, ArrayList<String[]> searchTokenArrayList) {
+
+		for (String temp : searchTokenList) {
+			String[] searchToken = new String[2];
+			searchToken = temp.split(":");
+			// ':'없는 통합검색일경우 토큰 형태 조정
+			  if(searchToken.length == 1) {
+				  String[] tempToken = new String[2];
+				  tempToken[0] = "통합검색";
+				  tempToken[1] = searchToken[0];
+				  searchToken = tempToken;
+			  }
+			searchTokenArrayList.add(searchToken);
+		
+		}
+		return searchTokenArrayList;
+	}
+
 }
+
