c2176c7ce21c95b2c6c6486b2a919b937e81646e
Bran <june20516@gmail.com>
2019-09-16 20:54:43 +0900

    itemService.java 파일명 대소문자 이슈2
    파일명 복구 후 재커밋 완료

--- src/main/java/com/fashionlog/model/service/ItemService.java
+++ src/main/java/com/fashionlog/model/service/ItemService.java
@@ -3,7 +3,7 @@ package com.fashionlog.model.service;
 import java.util.List;
 
 
-public interface ItemServiceTemp {
+public interface ItemService {
 	public List<Object> getItemPost(String itemName);
 	public Integer[] getSearchResult(List<String[]> searchTokenArrayList);
 }
