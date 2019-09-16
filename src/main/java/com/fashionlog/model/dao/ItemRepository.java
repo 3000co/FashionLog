5e36fa8f6547a91921e387fe862abfbb987273a9
haneol <n0netheless@naver.com>
2019-09-16 17:30:05 +0900

    선호 스타일-샘플 이미지 노출 완료
    선호 스타일 선정하는 동작 구현 중

--- src/main/java/com/fashionlog/model/dao/ItemRepository.java
+++ src/main/java/com/fashionlog/model/dao/ItemRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fashionlog.model.dto.Brand;
 import com.fashionlog.model.dto.Item;
 import com.fashionlog.model.dto.Post;
-import com.fashionlog.model.service.itemService;
+import com.fashionlog.model.service.ItemService;
 
-public interface ItemRepository extends JpaRepository<Item, Integer>, itemService{
+
+public interface ItemRepository extends JpaRepository<Item, Integer>, ItemService{
 	public List<Item> findByPostNoOrderByTagNoAsc(Post post);
 
 	
--- src/main/resources/static/js/styleSelect.js
+++ src/main/resources/static/js/styleSelect.js
@@ -3,17 +3,17 @@
 
 	//샘플 이미지 가져오기
 	$(document).ready(function(){
-		
-	  var gbl_data;
+		//좋아하는 스타일 목록
+		var FavoriteStyleArr = new Array(10);
 
 	  $.ajax({
 	   url : "/getFileList",
-	   async: false,
+	   async: true,
 	   type : 'POST',    
 	   dataType: "json",    
 	   success : function(data){
-	   gbl_data = data;
-	   
+		 
+	   setImgArray(data);
 	   },
 	   error: function(data){console.log("오류 발생"); console.log("data: "+data);},
 	   beforeSend: function() {
@@ -24,62 +24,67 @@
 	  });  
 	  
 	  //이미지 html에 넣는 부분
-	  console.log(gbl_data.sampleImgList[0].path);
-	  var imgArray = new Array(gbl_data.sampleImgList.length);
+	  function setImgArray(data){
+		  
+		  console.log(data);
+		  var length = data.sampleImgList.length;
+		  var sampleDataArray = new Array();
+		 
+		  //데이터 샘플을 배열에 저장
+		  for(var i=0; i< length-1; i++){
+	
+			  var sampleData = data.sampleImgList[i];
+			  sampleDataArray[i] = sampleData;
+
+		  }
+		  //배열 값 중 6개를 랜덤으로 선택한 뒤 지정된 위치에 넣음
+			var numArr = shuffleRandom();
+			var j=0
+			console.log(numArr);
+				for(var num of numArr){
+						var objImg = document.getElementById("styleImg"+j);
+						var styleName = sampleDataArray[num].name;
+						objImg.src = sampleDataArray[num].path;
+						$("<p>"+styleName+"</p>").prependTo($("#styleName"+j));
+						j++;
+				}
+		  
+		 }
 	 
-	  for(int j=0; j<=gbl_data.sampleImgList.length; j++){
-		  imgArray[j] = gbl_data.sampleImgList[j].path;
-	  }
-		/*imgArray[0] = "images/20190915073324_casual1.png";
-		imgArray[1] = "images/20190915073341_casual2.png";
-		imgArray[2] = "images/20190915073355_chic1.png"
-		imgArray[3] = "images/20190915073413_chic2.png";
-		imgArray[4] = "images/20190915073434_formal1.png"
-		imgArray[5] = "images/20190915073449_formal2.png";
-		*/
-		var numArr = shuffleRandom();
-		var i=0
-		console.log(numArr);
-			for(var num of numArr){
-				
-				if(typeof num == 'undefined'){	
-					continue;
-				}
-				else{
-					var objImg = document.getElementById("styleImg"+i);
-					objImg.src = imgArray[num];
-					i++;
-				}
-				
-			};
 	  
-	 });
+	  });
 	
 	
+	  
 	
+
 	function shuffleRandom(){
         var numArr = new Array(6);
-        var temp;
-        var rnum;
-       
-        //전달받은 매개변수 n만큼 배열 생성
-        for(var i=0; i<6; i++){
-            numArr.push(i);
+        
+        for(var i =0; i < 6; i++){
+        	numArr[i] = Math.floor(Math.random()*17);
+        	
+        	for(var j=0; j<i; j++){
+        		if(numArr[j] == numArr[i]) {
+        			i--;
+        			console.log("중복값. 뒤로 돌아갑니다.");
+        			console.log(numArr);
+        			break;
+        		}
+        	}
+        	
         }
- 
-        //값을 서로 섞기
-        for(var i=0; i< numArr.length ; i++)
-        {
-            rnum = Math.floor(Math.random() *6); //난수발생
-            temp = numArr[i];
-            numArr[i] = numArr[rnum];
-            numArr[rnum] = temp;
-        }
- 
-        return numArr;
-}
+   	 	return numArr;
+	}
 	
-			
-
-
-	 
+//	$(document).on('click',".styleSample", function(event){
+//		var styleName = $(this).children('label').html();
+//		console.log(styleName);
+//		
+//	});
+//	
+	function saveFavoriteStyle(){
+		var curNum;
+		var styleName = $(this).html;
+		console.log(styleName)
+	}
